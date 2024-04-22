4f7792ad685a72c5e4cf6f322c8082df782eb7e1
aryan <aryan.jand@gmail.com>
2024-04-22 14:17:05 -0700

    Formatting

--- two-pointers/two-sum-2-input-array-sorted.py
+++ two-pointers/two-sum-2-input-array-sorted.py
@@ -17,9 +17,9 @@ class Solution:
 sol = Solution()
 
 # Normal test cases
-print(sol.twoSum([2, 7, 11, 15], 9) == [1, 2]) # True
-print(sol.twoSum([3, 2, 4], 6) == [2, 3]) # False
-print(sol.twoSum([3, 3], 6) == [1, 2]) # True
-print(sol.twoSum([-1, 0], -1) == [1, 2]) # True
-print(sol.twoSum([-3, 4, 3, 90], 0) == [1, 3]) # True
-print(sol.twoSum([0, 0, 3, 4], 0) == [1, 2]) # True
+print(sol.twoSum([2, 7, 11, 15], 9) == [1, 2]) # Output: True
+print(sol.twoSum([3, 2, 4], 6) == [2, 3]) # Output: False
+print(sol.twoSum([3, 3], 6) == [1, 2]) # Output: True
+print(sol.twoSum([-1, 0], -1) == [1, 2]) # Output: True
+print(sol.twoSum([-3, 4, 3, 90], 0) == [1, 3]) # Output: True
+print(sol.twoSum([0, 0, 3, 4], 0) == [1, 2]) # Output: True
