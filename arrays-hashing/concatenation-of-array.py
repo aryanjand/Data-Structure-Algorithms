231f35280d723905ecf521015718d7a2789a1867
aryan <aryan.jand@gmail.com>
2024-04-25 21:36:18 -0700

    Added New Soultion to concatenation of arrays

--- arrays-hashing/concatenation-of-array.py
+++ arrays-hashing/concatenation-of-array.py
@@ -14,24 +14,39 @@ class Solution:
             nums.append(nums[i])
         return nums
 
+    def getConcatenation_solution_3(self, nums: List[int], n: int) -> List[int]:
+        # Time: O(n)
+        # Space: O(2n) -> O(n)
+        len_nums = len(nums)
+        for _ in range(n):
+            for i in range(len_nums):
+                nums.append(nums[i])
+        return nums
+
 def test_getConcatenation():
     solution = Solution()
     # Test with an empty list
     assert solution.getConcatenation_solution_1([]) == []
     assert solution.getConcatenation_solution_2([]) == []
+    assert solution.getConcatenation_solution_3([], 1) == []
 
     # Test with a single element
     assert solution.getConcatenation_solution_1([1]) == [1, 1]
     assert solution.getConcatenation_solution_2([1]) == [1, 1]
+    assert solution.getConcatenation_solution_3([1], 1) == [1, 1]
+
 
     # Test with multiple elements
     assert solution.getConcatenation_solution_1([1, 2, 3]) == [1, 2, 3, 1, 2, 3]
     assert solution.getConcatenation_solution_2([1, 2, 3]) == [1, 2, 3, 1, 2, 3]
+    assert solution.getConcatenation_solution_3([1, 2, 3], 1) == [1, 2, 3, 1, 2, 3]
+
 
     # Test with a large list
     nums = [i for i in range(10**6)]
     assert solution.getConcatenation_solution_1(nums) == nums + nums
     assert solution.getConcatenation_solution_2(nums[:]) == nums + nums
+    assert solution.getConcatenation_solution_3(nums[:], 1) == nums + nums
 
 test_getConcatenation()
 print("All Test Cases Passed!")
